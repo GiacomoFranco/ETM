251df3ecdfeb810cb4aa82e4cbe4412e786f9a75
Francoder_ <passiflora.freelance@gmail.com>
2025-10-15 00:07:22 -0500

    🚧 Adding layout components

---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Footer } from "../footer/footer";
+import { Header } from "../header/header";
+
+@Component({
+  selector: 'app-layout',
+  imports: [Footer, Header],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class Layout {
+
+}
