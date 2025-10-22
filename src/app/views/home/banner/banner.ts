d5bb8c3f896be92056aff382cbe0431de96467ca
Francoder_ <passiflora.freelance@gmail.com>
2025-10-21 19:29:08 -0500

    ✨ Adding banner section to home

---
+++ src/app/views/home/banner/banner.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { Button } from '@app/shared';
+
+@Component({
+  selector: 'app-banner',
+  imports: [Button],
+  templateUrl: './banner.html',
+  styleUrl: './banner.scss'
+})
+export class Banner {
+
+}
