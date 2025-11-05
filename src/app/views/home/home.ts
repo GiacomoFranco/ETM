eed68c8a88ca0d4f44f788c6143e927474316683
Francoder_ <passiflora.freelance@gmail.com>
2025-11-05 18:51:29 -0500

    ✨ Adding viewcase section

--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -4,10 +4,11 @@ import { SecondBanner } from "./second-banner/second-banner";
 import { ServicesSection, ClientsCarrousel} from '@shared';
 import { Banner } from './banner/banner';
 import { Reviews } from "./reviews/reviews";
+import { Viewcase } from "./viewcase/viewcase";
 
 @Component({
   selector: 'app-home',
-  imports: [Banner, ServicesSection, SecondBanner, ClientsCarrousel, Reviews],
+  imports: [Banner, ServicesSection, SecondBanner, ClientsCarrousel, Reviews, Viewcase],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
---
+++ src/app/views/home/viewcase/viewcase.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewcase',
+  imports: [],
+  templateUrl: './viewcase.html',
+  styleUrl: './viewcase.scss'
+})
+export class Viewcase {
+
+}
