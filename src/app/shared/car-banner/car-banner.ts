4c60c9b8d234189b57d94c680d1c05dfe01e9a7d
Francoder_ <passiflora.freelance@gmail.com>
2025-11-11 23:00:31 -0500

    🚧 Adding car-banner section

---
+++ src/app/shared/car-banner/car-banner.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Button } from "../button/button";
+
+@Component({
+  selector: 'app-car-banner',
+  imports: [Button],
+  templateUrl: './car-banner.html',
+  styleUrl: './car-banner.scss'
+})
+export class CarBanner {
+
+}
--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -8,10 +8,11 @@ import { Viewcase } from "./viewcase/viewcase";
 import { FaqSection } from "@app/shared/faq-section/faq-section";
 import { GalerySection } from "@app/shared/galery-section/galery-section";
 import { Locations } from "./locations/locations";
+import { CarBanner } from "@app/shared/car-banner/car-banner";
 
 @Component({
   selector: 'app-home',
-  imports: [Banner, ServicesSection, SecondBanner, ClientsCarrousel, Reviews, Viewcase, FaqSection, GalerySection, Locations],
+  imports: [Banner, ServicesSection, SecondBanner, ClientsCarrousel, Reviews, Viewcase, FaqSection, GalerySection, Locations, CarBanner],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
