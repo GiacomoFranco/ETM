6ed01fe83f31ec78150731afb40b06351232e122
Francoder_ <passiflora.freelance@gmail.com>
2025-11-16 12:46:45 -0500

    ✨ Adding scroll logic to navbar

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { Footer } from "./footer/footer";
 import { Header } from "./header/header";
 
@@ -6,8 +6,27 @@ import { Header } from "./header/header";
   selector: 'app-layout',
   imports: [Footer, Header],
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrl: './layout.scss',
 })
 export class Layout {
+  scrollTop = signal(0);
+  hideNav = signal(false);
+  navbarBackgroundOffset = 150;
+  navbarBackground = signal(false);
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(_event: Event) {
+    const scrollPosition =
+      window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const isScrollingDown = scrollPosition > this.scrollTop();
+    this.hideNav.set(isScrollingDown);
+
+    this.scrollTop.set(scrollPosition);
+
+    if (scrollPosition > this.navbarBackgroundOffset) {
+      this.navbarBackground.set(true);
+    } else {
+      this.navbarBackground.set(false);
+    }
+  }
 }
