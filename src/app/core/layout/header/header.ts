13e15e80f9f215f44eb01f1482292ddc1b9a7d5c
Francoder_ <passiflora.freelance@gmail.com>
2025-11-15 23:04:26 -0500

    ✨ Adding navbar

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { afterRenderEffect, Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Button } from "@app/shared";
+import { Icon } from "@app/shared/icon/icon";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Button, RouterLink, Icon, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  constructor() {
+    afterRenderEffect(() => {
+      this.openedMenu()
+        ? document?.body.classList.add('no-scroll')
+        : document?.body.classList.remove('no-scroll');
+      console.log('gere');
+    })
+  }
 
+  openedMenu = signal<boolean>(false)
+
+  toggleMenu() {
+    this.openedMenu.update(value => !value)
+  }
 }
