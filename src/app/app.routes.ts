5b1039054484e1f6e5e003757d4b58d8159739c8
Francoder_ <passiflora.freelance@gmail.com>
2026-01-11 15:31:01 -0500

    🚧 Preparing for deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,27 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    title: 'Inicio',
+    title: 'Inicio | EQUIPOS TECNI METÁLICOS S.A.S',
     path: '',
     loadComponent: () => import('@views').then((m) => m.Home),
   },
   {
-    title: 'Servicios',
+    title: 'Servicios | EQUIPOS TECNI METÁLICOS S.A.S',
     path: 'servicios',
     loadComponent: () => import('@views').then((m) => m.Services),
   },
   {
-    title: 'Sobre Nosotros',
+    title: 'Sobre Nosotros | EQUIPOS TECNI METÁLICOS S.A.S',
     path: 'sobre-nosotros',
     loadComponent: () => import('@views').then((m) => m.AboutUs),
   },
   {
-    title: 'Galería',
+    title: 'Galería | EQUIPOS TECNI METÁLICOS S.A.S',
     path: 'galeria',
     loadComponent: () => import('@views').then((m) => m.Galery),
   },
   {
-    title: 'Contacto',
+    title: 'Contacto | EQUIPOS TECNI METÁLICOS S.A.S',
     path: 'contacto',
     loadComponent: () => import('@views').then((m) => m.Contact),
   },
--- src/app/views/about-us/about-us.ts
+++ src/app/views/about-us/about-us.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { GeneralBanner } from "@app/shared/general-banner/general-banner";
+import { Pillars } from "./pillars/pillars";
+import { ClientsCarrousel } from "@app/shared";
+import { MisionVision } from "./mision-vision/mision-vision";
+import { GalerySection } from "@app/shared/galery-section/galery-section";
 
 @Component({
   selector: 'app-about-us',
-  imports: [GeneralBanner],
+  imports: [GeneralBanner, Pillars, ClientsCarrousel, MisionVision, GalerySection],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss',
 })
