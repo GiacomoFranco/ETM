26504b55b2aee3f7cb97e0b4e05e0274ac3146ca
Francoder_ <passiflora.freelance@gmail.com>
2026-02-20 00:33:21 -0500

    🚧 Adding multiple banners

--- src/app/shared/general-banner/general-banner.ts
+++ src/app/shared/general-banner/general-banner.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-general-banner',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './general-banner.html',
   styleUrl: './general-banner.scss',
 })
-export class GeneralBanner {}
+export class GeneralBanner {
+  backgroundImage = input<string>('/images/contact-banner.webp');
+  title = input<string>('Estamos aquí para brindarle soporte, confianza y atención oportuna.');
+  text = input<string>(
+    'Brindamos asesoría técnica y atención personalizada para resolver sus requerimientos con rapidez y precisión. Nuestro equipo ofrece soporte en sitio y remoto, coordinación de visitas, cotizaciones claras y seguimiento oportuno, respaldado por repuestos disponibles, talleres especializados y cobertura nacional para garantizar la continuidad de sus operaciones.',
+  );
+}
