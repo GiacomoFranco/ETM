f7e20ff16096037e16560767e67ab5e7a66765e0
Francoder_ <passiflora.freelance@gmail.com>
2026-03-01 19:50:21 -0500

    🚧 Adding logo explanation

--- src/app/views/about-us/about-us.ts
+++ src/app/views/about-us/about-us.ts
@@ -4,12 +4,13 @@ import { ClientsCarrousel } from '@app/shared';
 import { GalerySection } from '@app/shared/galery-section/galery-section';
 import { GeneralBanner } from '@app/shared/general-banner/general-banner';
 
+import { LogoExplanation } from './logo-explanation/logo-explanation';
 import { MisionVision } from './mision-vision/mision-vision';
 import { Pillars } from './pillars/pillars';
 
 @Component({
   selector: 'app-about-us',
-  imports: [GeneralBanner, Pillars, ClientsCarrousel, MisionVision, GalerySection],
+  imports: [GeneralBanner, Pillars, LogoExplanation, ClientsCarrousel, MisionVision, GalerySection],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss',
 })
---
+++ src/app/views/about-us/logo-explanation/logo-explanation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-explanation',
+  imports: [],
+  templateUrl: './logo-explanation.html',
+  styleUrl: './logo-explanation.scss'
+})
+export class LogoExplanation {
+
+}
